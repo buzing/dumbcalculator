f3e0b62adb20162a07d2ea59dd738191002050c0
buzing <example@gamesense.pub>
2026-05-09 17:47:59 +0200

    awesome

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,105 @@ namespace wowdumbcalculator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtWishedAmt = new System.Windows.Forms.TextBox();
+            this.txtCurrentAmt = new System.Windows.Forms.TextBox();
+            this.txtGamesAmt = new System.Windows.Forms.TextBox();
+            this.lblGamesAmt = new System.Windows.Forms.Label();
+            this.lblWishedAmt = new System.Windows.Forms.Label();
+            this.lblCurrentAmt = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtWishedAmt
+            // 
+            this.txtWishedAmt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtWishedAmt.Location = new System.Drawing.Point(152, 12);
+            this.txtWishedAmt.Name = "txtWishedAmt";
+            this.txtWishedAmt.Size = new System.Drawing.Size(80, 20);
+            this.txtWishedAmt.TabIndex = 0;
+            // 
+            // txtCurrentAmt
+            // 
+            this.txtCurrentAmt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCurrentAmt.Location = new System.Drawing.Point(152, 38);
+            this.txtCurrentAmt.Name = "txtCurrentAmt";
+            this.txtCurrentAmt.Size = new System.Drawing.Size(80, 20);
+            this.txtCurrentAmt.TabIndex = 1;
+            this.txtCurrentAmt.Tag = "";
+            // 
+            // txtGamesAmt
+            // 
+            this.txtGamesAmt.Location = new System.Drawing.Point(192, 64);
+            this.txtGamesAmt.Name = "txtGamesAmt";
+            this.txtGamesAmt.ReadOnly = true;
+            this.txtGamesAmt.Size = new System.Drawing.Size(40, 20);
+            this.txtGamesAmt.TabIndex = 2;
+            // 
+            // lblGamesAmt
+            // 
+            this.lblGamesAmt.AutoSize = true;
+            this.lblGamesAmt.Location = new System.Drawing.Point(97, 67);
+            this.lblGamesAmt.Name = "lblGamesAmt";
+            this.lblGamesAmt.Size = new System.Drawing.Size(89, 13);
+            this.lblGamesAmt.TabIndex = 3;
+            this.lblGamesAmt.Text = "Amount of games";
+            // 
+            // lblWishedAmt
+            // 
+            this.lblWishedAmt.AutoSize = true;
+            this.lblWishedAmt.Location = new System.Drawing.Point(12, 14);
+            this.lblWishedAmt.Name = "lblWishedAmt";
+            this.lblWishedAmt.Size = new System.Drawing.Size(120, 13);
+            this.lblWishedAmt.TabIndex = 4;
+            this.lblWishedAmt.Text = "Wished amount of items";
+            // 
+            // lblCurrentAmt
+            // 
+            this.lblCurrentAmt.AutoSize = true;
+            this.lblCurrentAmt.Location = new System.Drawing.Point(12, 38);
+            this.lblCurrentAmt.Name = "lblCurrentAmt";
+            this.lblCurrentAmt.Size = new System.Drawing.Size(118, 13);
+            this.lblCurrentAmt.TabIndex = 5;
+            this.lblCurrentAmt.Text = "Current amount of items";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(12, 61);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 6;
+            this.btnCalc.Text = "run calc";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(244, 91);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblCurrentAmt);
+            this.Controls.Add(this.lblWishedAmt);
+            this.Controls.Add(this.lblGamesAmt);
+            this.Controls.Add(this.txtGamesAmt);
+            this.Controls.Add(this.txtCurrentAmt);
+            this.Controls.Add(this.txtWishedAmt);
+            this.Name = "Form1";
+            this.Text = "Dumb calcualtor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtWishedAmt;
+        private System.Windows.Forms.TextBox txtCurrentAmt;
+        private System.Windows.Forms.TextBox txtGamesAmt;
+        private System.Windows.Forms.Label lblGamesAmt;
+        private System.Windows.Forms.Label lblWishedAmt;
+        private System.Windows.Forms.Label lblCurrentAmt;
+        private System.Windows.Forms.Button btnCalc;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,41 @@ namespace wowdumbcalculator
         {
             InitializeComponent();
         }
+
+        /*
+         * variables:
+         * wished amount of items
+         * current amount of items
+         * remaining items until wished amount
+         * games until wished amount (assuming you get 5 drops)
+         */
+
+        int nWishedAmt, nCurrentAmt, nRemaining, nGamesAmt;
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            txtGamesAmt.Clear();
+            try
+            {
+                nWishedAmt = Convert.ToInt32(txtWishedAmt.Text);
+                nCurrentAmt = Convert.ToInt32(txtCurrentAmt.Text);
+                if (nWishedAmt < nCurrentAmt)
+                {
+                    MessageBox.Show("current amount is bigger than wished amount");
+                } else if ( nWishedAmt == nCurrentAmt)
+                {
+                    MessageBox.Show("wished amount is the same as current amount");
+                } else
+                {
+                    nRemaining = nWishedAmt - nCurrentAmt;
+                    nGamesAmt = nRemaining / 5;
+                    txtGamesAmt.Text = nGamesAmt.ToString();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("hello wrong input or something");
+            }
+        }
     }
 }
